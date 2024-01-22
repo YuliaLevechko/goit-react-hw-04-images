f1189d8e78ab680a89d20a87c8815c26e12784ef
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2024-01-22 17:06:11 +0200

    update searchbar and app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { fetchImages } from './feachImage/FeachImage';
 
 import Button from './Button';
@@ -7,88 +7,87 @@ import Searchbar from './Searchbar';
 import Notiflix from 'notiflix';
 import Loader from './Loader';
 
-let page = 1;
-
 const App = () => {
-const [inputData , setInputData ] = useState('');
-const [  items , setItems ] = useState([]);
-const [ status , setStatus ] = useState('idle')
-const [  totalHits , setTotalHits ] = useState(0)
+  const [inputData, setInputData] = useState('');
+  const [items, setItems] = useState([]);
+  const [status, setStatus] = useState('idle');
+  const [totalHits, setTotalHits] = useState(0);
+  const [page, setPage] = useState(1);
 
-const handleSubmit = async (inputData )=> {
-  page = 1;
-  if (inputData.trim() === '') {
-    Notiflix.Notify.info('You cannot search by empty field, try again.');
-    return;
-  } else {
-    try {
-      setStatus('pending');
-      const { totalHits, hits } = await fetchImages(inputData, page);
-      if (hits.length < 1) {
-        setStatus( 'idle' );
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        setItems( hits)
-        setInputData(inputData)
-        setTotalHits(totalHits)
-        setStatus('resolved')
+  const handleSubmit = async (inputData) => {
+    setPage(1);
+    if (inputData.trim() === '') {
+      Notiflix.Notify.info('You cannot search by an empty field, try again.');
+      return;
+    } else {
+      try {
+        setStatus('pending');
+        const { totalHits, hits } = await fetchImages(inputData, 1);
+        if (hits.length < 1) {
+          setStatus('idle');
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else {
+          setItems(hits);
+          setInputData(inputData);
+          setTotalHits(totalHits);
+          setStatus('resolved');
+        }
+      } catch (error) {
+        setStatus('rejected');
       }
-    } catch (error) {
-      setStatus('rejected')
     }
+  };
+
+  const onNextPage = async () => {
+    setStatus('pending');
+    try {
+      const { hits } = await fetchImages(inputData, page + 1);
+      setItems((prevState) => [...prevState, ...hits]);
+      setPage((prevPage) => prevPage + 1);
+      setStatus('resolved');
+    } catch (error) {
+      setStatus('rejected');
+    }
+  };
+
+  if (status === 'idle') {
+    return (
+      <div>
+        <Searchbar onSubmit={handleSubmit} />
+      </div>
+    );
+  }
+  if (status === 'pending') {
+    return (
+      <div>
+        <Searchbar onSubmit={handleSubmit} />
+        <ImageGallery page={page} items={items} />
+        <Loader />
+        {totalHits > 12 && <Button onClick={onNextPage} />}
+      </div>
+    );
+  }
+  if (status === 'rejected') {
+    return (
+      <div>
+        <Searchbar onSubmit={handleSubmit} />
+        <p>Something wrong, try later</p>
+      </div>
+    );
+  }
+  if (status === 'resolved') {
+    return (
+      <div>
+        <Searchbar onSubmit={handleSubmit} />
+        <ImageGallery page={page} items={items} />
+        {totalHits > 12 && totalHits > items.length && (
+          <Button onClick={onNextPage} />
+        )}
+      </div>
+    );
   }
 };
 
-const onNextPage = async () => {
-  setStatus('pending');
-
-  try {
-    const { hits } = await fetchImages(inputData, (page += 1));
-    setItems((prevState)=>[...prevState, ...hits]);
-    setStatus( 'resolved');
-  } catch (error) {
-    setStatus('rejected');
-  }
-};
-
-
-if (status === 'idle') {
-  return (
-    <div>
-      <Searchbar onSubmit={handleSubmit} />
-    </div>
-  );
-}
-if (status === 'pending') {
-  return (
-    <div>
-      <Searchbar onSubmit={handleSubmit} />
-      <ImageGallery page={page} items={items} />
-      <Loader />
-      {totalHits > 12 && <Button onClick={onNextPage} />}
-    </div>
-  );
-}
-if (status === 'rejected') {
-  return (
-    <div>
-      <Searchbar onSubmit={handleSubmit} />
-      <p>Something wrong, try later</p>
-    </div>
-  );
-}
-if (status === 'resolved') {
-  return (
-    <div>
-      <Searchbar onSubmit={handleSubmit} />
-      <ImageGallery page={page} items={items} />
-      {totalHits > 12 && totalHits > items.length && (
-        <Button onClick={onNextPage} />
-      )}
-    </div>
-  );
-}
-}
 export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,41 +1,48 @@
-import  { useState } from 'react'
+import { useState } from 'react';
 import Notiflix from 'notiflix';
 
-import css from  './Searchbar.module.css'
+import css from './Searchbar.module.css';
 
-const Searchbar =  ({onSubmit}) =>{
-const [ inputvalue ,setInputvalue] = useState('');
+const Searchbar = ({ onSubmit }) => {
+  const [inputvalue, setInputvalue] = useState('');
 
- const hendleSubmit = (e)=>{
-  e.preventDefault () 
-  if( inputvalue.trim() === ''){
-     Notiflix.Notify.failure('please writing value ');
-  }
-  onSubmit(inputvalue);
-  setInputvalue('')
-}
+  const hendleSubmit = (e) => {
+    e.preventDefault();
+    if (inputvalue.trim() === '') {
+      Notiflix.Notify.failure('please writing value ');
+    } else {
+      onSubmit(inputvalue);
+      setInputvalue('');
+    }
+  };
 
- const hendleChange = (e) =>{
-  setInputvalue( e.currentTarget.value.toLowerCase())
-}
-return (
-  <header className={css.searchbar}>
-  <form  onSubmit={ hendleSubmit}   className={css.form}>
-    <input  
-    onChange={hendleChange}
-      className={css.input}
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
-    />
-      <button type="submit" className={css.button}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1200px-Search_Icon.svg.png" width="30px" height="30px" alt=""/>
-    </button>
-  </form>
-</header>
-)
+  const hendleChange = (e) => {
+    setInputvalue(e.currentTarget.value.toLowerCase());
+  };
 
-}
+  return (
+    <header className={css.searchbar}>
+      <form onSubmit={hendleSubmit} className={css.form}>
+        <input
+          onChange={hendleChange}
+          value={inputvalue}
+          className={css.input}
+          type="text"
+          autoComplete="off"
+          autoFocus={false}
+          placeholder="Search images and photos"
+        />
+        <button type="submit" className={css.button}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1200px-Search_Icon.svg.png"
+            width="30px"
+            height="30px"
+            alt=""
+          />
+        </button>
+      </form>
+    </header>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
