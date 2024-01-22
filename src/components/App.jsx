2a276925467b6eaa8bee72082607f8610a8cee7c
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2024-01-22 17:18:19 +0200

    update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchImages } from './feachImage/FeachImage';
 
 import Button from './Button';
@@ -14,12 +14,12 @@ const App = () => {
   const [totalHits, setTotalHits] = useState(0);
   const [page, setPage] = useState(1);
 
-  const handleSubmit = async (inputData) => {
-    setPage(1);
-    if (inputData.trim() === '') {
-      Notiflix.Notify.info('You cannot search by an empty field, try again.');
-      return;
-    } else {
+  useEffect(() => {
+    const fetchData = async () => {
+      if (inputData.trim() === '') {
+        return;
+      }
+
       try {
         setStatus('pending');
         const { totalHits, hits } = await fetchImages(inputData, 1);
@@ -30,19 +30,20 @@ const App = () => {
           );
         } else {
           setItems(hits);
-          setInputData(inputData);
           setTotalHits(totalHits);
           setStatus('resolved');
         }
       } catch (error) {
         setStatus('rejected');
       }
-    }
-  };
+    };
+
+    fetchData();
+  }, [inputData]); 
 
   const onNextPage = async () => {
-    setStatus('pending');
     try {
+      setStatus('pending');
       const { hits } = await fetchImages(inputData, page + 1);
       setItems((prevState) => [...prevState, ...hits]);
       setPage((prevPage) => prevPage + 1);
@@ -52,42 +53,21 @@ const App = () => {
     }
   };
 
-  if (status === 'idle') {
-    return (
-      <div>
-        <Searchbar onSubmit={handleSubmit} />
-      </div>
-    );
-  }
-  if (status === 'pending') {
-    return (
-      <div>
-        <Searchbar onSubmit={handleSubmit} />
-        <ImageGallery page={page} items={items} />
-        <Loader />
-        {totalHits > 12 && <Button onClick={onNextPage} />}
-      </div>
-    );
-  }
-  if (status === 'rejected') {
-    return (
-      <div>
-        <Searchbar onSubmit={handleSubmit} />
-        <p>Something wrong, try later</p>
-      </div>
-    );
-  }
-  if (status === 'resolved') {
-    return (
-      <div>
-        <Searchbar onSubmit={handleSubmit} />
-        <ImageGallery page={page} items={items} />
-        {totalHits > 12 && totalHits > items.length && (
-          <Button onClick={onNextPage} />
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Searchbar onSubmit={setInputData} />
+      {status === 'pending' && <Loader />}
+      {status === 'rejected' && <p>Something wrong, try later</p>}
+      {status === 'resolved' && (
+        <div>
+          <ImageGallery page={page} items={items} />
+          {totalHits > 12 && totalHits > items.length && (
+            <Button onClick={onNextPage} />
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
